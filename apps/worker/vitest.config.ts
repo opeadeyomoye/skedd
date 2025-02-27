a0abc5a5b3071711b51cd250dc40a2500f030545
Ope Adeyomoye <victoradeyomoye@yahoo.co.uk>
2025-02-27 23:46:30 +0100

    [wrkr] optimise @clerk/backend for test runs

--- apps/worker/vitest.config.ts
+++ apps/worker/vitest.config.ts
@@ -3,6 +3,14 @@ import { defineWorkersConfig } from '@cloudflare/vitest-pool-workers/config'
 export default defineWorkersConfig({
   assetsInclude: ['./.tmp/drizzle/*.sql'],
   test: {
+    deps: {
+      optimizer: {
+        ssr: {
+          enabled: true,
+          include: ['@clerk/backend'],
+        }
+      },
+    },
     poolOptions: {
       workers: {
         wrangler: { configPath: './wrangler.jsonc', environment: 'test' },
