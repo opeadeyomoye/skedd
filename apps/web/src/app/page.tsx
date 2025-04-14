894cf767bccfd50259625ba055bfe503d863ee1c
Ope Adeyomoye <victoradeyomoye@yahoo.co.uk>
2025-04-15 00:03:33 +0100

    [web] link GitHub

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -11,10 +11,9 @@ import {
 import { Dialog, DialogPanel } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
-import { TouchTarget } from '@/components/button'
 
-const navigation = [
-  { name: ' ', href: '#' },
+const navigation: { name: string, href: string }[] = [
+  // { name: ' ', href: '#' },
   // { name: 'Features', href: '#' },
   // { name: 'Marketplace', href: '#' },
   // { name: 'Company', href: '#' },
@@ -44,10 +43,18 @@ export default function Page() {
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-sm/6 font-semibold text-gray-900">
+              <a key={item.name} href={item.href} className="text-sm/6 font-semibold text-gray-200">
                 {item.name}
               </a>
             ))}
+            <a
+              className="text-sm/6 font-semibold text-gray-200"
+              href="https://github.com/opeadeyomoye/skedd"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Star on GitHub
+            </a>
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
             <SignedOut>
@@ -64,33 +71,41 @@ export default function Page() {
         </nav>
         <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
           <div className="fixed inset-0 z-50" />
-          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-slate-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
+                <span className="sr-only">skedd</span>
                 <img
                   alt=""
-                  src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                  className="h-8 w-auto"
+                  src="/skedd-icon.png"
+                  className="h-8 w-auto rounded-full"
                 />
               </a>
               <button
                 type="button"
                 onClick={() => setMobileMenuOpen(false)}
-                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                className="-m-2.5 rounded-md p-2.5 text-gray-400"
               >
                 <span className="sr-only">Close menu</span>
                 <XMarkIcon aria-hidden="true" className="size-6" />
               </button>
             </div>
             <div className="mt-6 flow-root">
-              <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="-my-6 divide-y divide-slate-500/10">
                 <div className="space-y-2 py-6">
+                  <a
+                    href="https://github.com/opeadeyomoye/skedd"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-slate-100 hover:bg-slate-800"
+                  >
+                    Star on GitHub
+                  </a>
                   {navigation.map((item) => (
                     <a
                       key={item.name}
                       href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-slate-100 hover:bg-slate-800"
                     >
                       {item.name}
                     </a>
@@ -100,7 +115,7 @@ export default function Page() {
                   <SignedOut>
                     <SignInButton>
                       <span
-                        className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                        className="-mx-3 block rounded-lg px-3 py-2.5 cursor-pointer text-base/7 font-semibold text-slate-100 hover:bg-slate-800"
                       >
                         Log in
                       </span>
